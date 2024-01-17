026d2258e0899f5d4e0550b72b32fdde5c706af3
MJoonu <wnsdn8009@gmail.com>
2024-01-17 17:09:54 +0900

    change line

--- src/main/java/hello/login/domain/login/LoginService.java
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -22,6 +22,7 @@ public class LoginService {
 //            return null;
 //        }
 
-        return memberRepository.findByLoginId(loginId).filter(m -> m.getPassword().equals(password)).orElse(null);
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password)).orElse(null);
     }
 }
